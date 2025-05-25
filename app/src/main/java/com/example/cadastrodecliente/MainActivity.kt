02126306160fe0c65b6a73b78e18af604c4fa922
caramelPopp <rampazibarbara1203@gmail.com>
2025-05-24 21:57:09 -0300

    finish reading data & app appearance

--- app/src/main/java/com/example/cadastrodecliente/MainActivity.kt
+++ app/src/main/java/com/example/cadastrodecliente/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.cadastrodecliente
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,16 +9,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,7 +37,6 @@ import androidx.compose.ui.text.input.KeyboardType.Companion.Phone
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
 import com.example.cadastrodecliente.ui.theme.CadastroDeClienteTheme
 import com.example.cadastrodecliente.ui.theme.Purple40
@@ -58,146 +61,230 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     val db = Firebase.firestore
-    var exibition = ""
 
+    // Tela principal da aplicação, que aparece antes de qualquer outra
+    var currentScreen by remember { mutableStateOf("home") }
+
+    // Registrando os dados necessários para a navegação interna entre telas
+    when (currentScreen) {
+        /*
+        - 'onBack' levará da tela do momento de volta para a Home.
+        - 'onNavigateToRegister' levará a tela de cadastro.
+        - 'onNavigateToVisualize' levará a tela de listagem de usuários.
+        - A variável de instância do banco só é criada aqui, mas estará presente
+        nas outras telas como parâmetro.
+        */
+
+        "home" -> HomeScreen(
+            onNavigateToRegister = { currentScreen = "cadastro" }
+        )
+
+        "cadastro" -> RegisterScreen(
+            onNavigateToVisualize = { currentScreen = "visualizar" },
+            onBack = { currentScreen = "home" },
+            db = db
+        )
+
+        "visualizar" -> VisualizeScreen(
+            onBack = { currentScreen = "home" },
+            db = db
+        )
+    }
+
+}
+
+@Composable
+fun HomeScreen(
+    onNavigateToRegister: () -> Unit
+) {
+    Column(
+        Modifier.background(Color.Transparent).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Cadastro de Cliente", fontSize = 35.sp, color = Purple40)
+        Spacer(modifier = Modifier.height(28.dp))
+        Button(
+            onClick = onNavigateToRegister,
+            colors = ButtonDefaults.buttonColors(containerColor = Purple40)
+        ) {
+            Text("Cadastrar-se")
+        }
+    }
+}
+
+@Composable
+fun RegisterScreen(
+    onNavigateToVisualize: () -> Unit,
+    onBack: () -> Unit,
+    db: com.google.firebase.firestore.FirebaseFirestore
+) {
     var name by remember { mutableStateOf("") }
     var birth by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var phone by remember { mutableStateOf("") }
     var address by remember { mutableStateOf("") }
 
-    Column (
-        Modifier.background(Color.Transparent).fillMaxSize()
+    Column(
+        Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            Modifier.fillMaxWidth().padding(20.dp)
-        ) {
-
-        }
-        Row(
-            Modifier.fillMaxWidth().padding(20.dp),
-            Arrangement.Center
-        ){
-            Text("Crie sua conta!", fontFamily = FontFamily.Default, fontSize = 35.sp, color = Purple40)
-        }
-        Row(
-            Modifier.fillMaxWidth().padding(20.dp),
-            Arrangement.Center
-        ){
-            OutlinedTextField(
-                value = name,
-                onValueChange = { name = it },
-                label = { Text("Nome:") }
-            )
-        }
-        Row(
-            Modifier.fillMaxWidth().padding(20.dp),
-            Arrangement.Center
-        ){
-            OutlinedTextField(
-                value = birth,
-                onValueChange = { birth = it },
-                label = { Text("Data de Nascimento:") },
-                singleLine = true
-            )
-        }
-        Row(
-            Modifier.fillMaxWidth().padding(20.dp),
-            Arrangement.Center
-        ){
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("E-mail:") },
-                maxLines = 2,
-                keyboardOptions = KeyboardOptions(keyboardType = Email)
-            )
-        }
+        Spacer(modifier = Modifier.height(20.dp))
         Row(
             Modifier.fillMaxWidth().padding(20.dp),
             Arrangement.Center
         ) {
-            OutlinedTextField(
-                value = phone,
-                onValueChange = { phone = it },
-                label = { Text("Telefone:") },
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(keyboardType = Phone)
-            )
+            Text("Cadastre-se!", fontSize = 32.sp, color = Purple40)
         }
-        Row(
-            Modifier.fillMaxWidth().padding(20.dp),
-            Arrangement.Center
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Campo 'Nome'
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Nome") })
+
+        // Campo 'Data de Nascimento'
+        OutlinedTextField(
+            value = birth,
+            onValueChange = { birth = it },
+            label = { Text("Data de Nascimento")})
+
+        // Campo 'Email'
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            keyboardOptions = KeyboardOptions(keyboardType = Email)
+        )
+
+        // Campo 'Telefone'
+        OutlinedTextField(
+            value = phone,
+            onValueChange = { phone = it },
+            label = { Text("Telefone") },
+            keyboardOptions = KeyboardOptions(keyboardType = Phone)
+        )
+
+        // Campo 'Endereço'
+        OutlinedTextField(
+            value = address,
+            onValueChange = { address = it },
+            label = { Text("Endereço") })
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                val user = hashMapOf(
+                    "nome" to name,
+                    "nascimento" to birth,
+                    "email" to email,
+                    "telefone" to phone,
+                    "endereco" to address
+                )
+                db.collection("users").add(user)
+
+                // Limpa campos
+                name = ""
+                birth = ""
+                email = ""
+                phone = ""
+                address = ""
+
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Purple40)
         ) {
-            OutlinedTextField(
-                value = address,
-                onValueChange = { address = it },
-                label = { Text("Endereço:") }
-            )
+            Text("Salvar")
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = onNavigateToVisualize,
+            colors = ButtonDefaults.buttonColors(containerColor = Purple40)
+        ) {
+            Text("Listar Usuários")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
         Row(
             Modifier.fillMaxWidth().padding(20.dp),
-            Arrangement.Center
-        ){
-            Button(
-                onClick = {
-                    val user = hashMapOf(
-                        "nome" to name,
-                        "nascimento" to birth,
-                        "email" to email,
-                        "telefone" to phone,
-                        "endereco" to address
-                    )
-
-                    db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener { documentReference ->
-                            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-                        }
-                        .addOnFailureListener { e ->
-                            Log.w(TAG, "Error adding document", e)
-                        }
-
-                    name = ""
-                    birth = ""
-                    email = ""
-                    phone = ""
-                    address = ""
-                },
-                colors = ButtonDefaults.buttonColors(containerColor = Purple40)
-            ) {
-                Text("Cadastrar")
-            }
-        }
-        Row(
-            Modifier.fillMaxWidth().padding(20.dp)
+            Arrangement.Absolute.Left
         ) {
             Button(
-                onClick = {
-                    db.collection("users")
-                        .get()
-                        .addOnSuccessListener { result ->
-                            for (document in result) {
-                                exibition += Log.d(TAG, "${document.id} => ${document.data}").toString()
-                            }
-                        }
-                        .addOnFailureListener { exception ->
-                            exibition = Log.w(TAG, "Error getting documents.", exception).toString()
-                        }
-                },
+                onClick = onBack,
                 colors = ButtonDefaults.buttonColors(containerColor = Purple40)
             ) {
-                Text("Verificar Registros")
+                Text("Voltar")
             }
-        }
-        Row(
-            Modifier.fillMaxWidth().padding(20.dp)
-        ){
-            Text(exibition)
         }
     }
 }
 
+@Composable
+fun VisualizeScreen(
+    onBack: () -> Unit,
+    db: com.google.firebase.firestore.FirebaseFirestore
+) {
+    var answer by remember { mutableStateOf("") }
+
+
+    LaunchedEffect(Unit) {
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                val builder = StringBuilder()
+                for (document in result) {
+                    val name = document.getString("nome") ?: ""
+                    val birth = document.getString("nascimento") ?: ""
+                    val email = document.getString("email") ?: ""
+                    val phone = document.getString("telefone") ?: ""
+                    val address = document.getString("endereco") ?: ""
+
+                    builder.append(
+                        """
+                        Nome: $name
+                        Nascimento: $birth
+                        Email: $email
+                        Telefone: $phone
+                        Endereço: $address
+                        """.trimIndent()
+                    )
+                    builder.append("\n\n--------------------------------------\n\n")
+                }
+                answer = builder.toString()
+            }
+    }
+
+    Column(
+        Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(16.dp),
+        verticalArrangement = Arrangement.Top
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            Modifier.fillMaxWidth().padding(20.dp),
+            Arrangement.Center
+        ) {
+            Text("Usuários Cadastrados", fontSize = 32.sp, color = Purple40)
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(answer, fontSize = 18.sp)
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Button(
+            onClick = onBack,
+            colors = ButtonDefaults.buttonColors(containerColor = Purple40)
+        ) {
+            Text("Voltar")
+        }
+
+        Spacer(modifier = Modifier.height(28.dp))
+    }
+}
 
 @Preview
 @Composable
